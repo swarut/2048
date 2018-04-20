fb9256a8662e1a485c8f357b5bf8312d474812df
Warut Surapat <s.warut@gmail.com>
2018-04-20 23:29:17 +0900

    Add action.

---
+++ src/actions/index.js
@@ -0,0 +1,34 @@
+export const START = 'START'
+export const start = () => {
+  return {
+    type: START
+  }
+}
+
+export const MOVE_LEFT = 'MOVE_LEFT'
+export const MOVE_LEFT = () => {
+  return {
+    type: MOVE_LEFT
+  }
+}
+
+export const MOVE_RIGHT = 'MOVE_RIGHT'
+export const MOVE_RIGHT = () => {
+  return {
+    type: MOVE_RIGHT
+  }
+}
+
+export const MOVE_UP = 'MOVE_UP'
+export const MOVE_UP = () => {
+  return {
+    type: MOVE_UP
+  }
+}
+
+export const MOVE_DOWN = 'MOVE_DOWN'
+export const MOVE_DOWN = () => {
+  return {
+    type: MOVE_DOWN
+  }
+}
