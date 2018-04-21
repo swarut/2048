2cad907d9dc61b17e09516746dbd5d2b4913e9f6
Warut Surapat <s.warut@gmail.com>
2018-04-21 11:34:15 +0900

    Update style.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from '../reducers/index'
-
+import '../css/app.css'
 import Board from './Board'
 
 let store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
