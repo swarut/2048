aec7a10b5c199118363e56c524c865c209dd3620
Warut Surapat <s.warut@gmail.com>
2018-04-25 19:01:03 +0900

    Add location randomize function.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,12 @@ const defaultState = {
   isStarted: false
 }
 
+const randomLocation = (row = 4, col = 4) => {
+  let rRow = Math.floor(Math.random() * row)
+  let rCol = Math.floor(Math.random() * col)
+  return {row: rRow, col: rCol}
+}
+
 const reducer = (state = defaultState, action) => {
   switch(action.type) {
     case START:
