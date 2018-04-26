42587db247b123a5382007378c26f298de75c470
Warut Surapat <s.warut@gmail.com>
2018-04-26 22:59:39 +0900

    Add startupcells.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,23 @@
 import { START } from '../actions/index'
 
+const startupCells = new Array(4).fill(
+  (new Array(4)).fill(null)
+)
+
 const defaultState = {
-  isStarted: false
+  isStarted: false,
+  cells: startupCells
 }
 
 const randomLocation = (row = 4, col = 4) => {
   let rRow = Math.floor(Math.random() * row)
   let rCol = Math.floor(Math.random() * col)
   return {row: rRow, col: rCol}
+}
+
+const initializeCells = (startupCells) => {
+  {row, col} = randomLocation()
+  
 }
 
 const reducer = (state = defaultState, action) => {
