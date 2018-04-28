92de634a753a0985c758b33abcb03bd626fdaff0
Warut Surapat <s.warut@gmail.com>
2018-04-28 11:56:00 +0900

    Add old index as ref.

---
+++ src/reducers/index_old.js
@@ -0,0 +1,34 @@
+import { START } from '../actions/index'
+
+const startupCells = new Array(4).fill(
+  (new Array(4)).fill(null)
+)
+
+const defaultState = {
+  isStarted: false,
+  cells: startupCells
+}
+
+const randomLocation = (row = 4, col = 4) => {
+  let rRow = Math.floor(Math.random() * row)
+  let rCol = Math.floor(Math.random() * col)
+  return {row: rRow, col: rCol}
+}
+
+const initializeCells = (startupCells) => {
+  let {row, col} = randomLocation()
+  
+}
+
+const reducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case START:
+      return {
+        isStarted: true
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
