30f43bbb275f513633a2f3d26f0de36d10c63f14
Warut Surapat <s.warut@gmail.com>
2018-04-28 11:55:49 +0900

    Switch to new idea implementation.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,48 @@
 import { START } from '../actions/index'
 
-const startupCells = new Array(4).fill(
-  (new Array(4)).fill(null)
-)
+const startupCells = (new Array(16)).fill(null)
 
 const defaultState = {
   isStarted: false,
   cells: startupCells
 }
 
-const randomLocation = (row = 4, col = 4) => {
-  let rRow = Math.floor(Math.random() * row)
-  let rCol = Math.floor(Math.random() * col)
-  return {row: rRow, col: rCol}
+const randomlyAddCell = (cells) => {
+
+  let possiblePositions = cells.reduce((acc, item, index) => {
+    if(item === null) {
+      acc.push(index)
+    }
+    return acc
+  }, [])
+
+  let randomIndex = Math.floor(Math.random() * possiblePositions.length)
+  return cells.map((item, index) => {
+    if(index === possiblePositions[randomIndex]) {
+      return 2
+    }
+    return item
+  })
 }
 
-const initializeCells = (startupCells) => {
-  let {row, col} = randomLocation()
-  
+const twoDimensionCells = (cells, width = 4) => {
+  return cells.reduce((acc, item, index) => {
+    if((index % width) == 0 ) {
+      acc.push([item])
+    }
+    else {
+      let lastIndex = acc.length - 1
+      acc[lastIndex].push(item)
+    }
+    return acc
+  }, [])
+}
+
+// Return co-ordinate in 2 dimension for an index.
+const getCoordinate = (cellIndex, width = 4) => {
+  let row = Math.floor(cellIndex/width)
+  let col = cellIndex % width
+  return { row, col }
 }
 
 const reducer = (state = defaultState, action) => {
