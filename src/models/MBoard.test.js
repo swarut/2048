d3674f8ddfbeca48557c4aede6b48a37a2068fe3
Warut Surapat <s.warut@gmail.com>
2018-04-28 23:54:51 +0900

    Add test for matrixize.

--- src/models/MBoard.test.js
+++ src/models/MBoard.test.js
@@ -22,4 +22,31 @@ describe('MBoard', () => {
       expect(cellsWithElementAfterTwoTimesModified).toBe(2)
     })
   })
+
+  describe('matrixize', () => {
+    test('creates a 2 dimension array from a single dimension array', () => {
+      let cells = (new Array(16)).fill(null)
+      let matrix = matrixize(cells)
+
+      expect(matrix.length).toBe(4)
+      expect(matrix[0].length).toBe(4)
+      expect(matrix[1].length).toBe(4)
+      expect(matrix[2].length).toBe(4)
+      expect(matrix[3].length).toBe(4)
+    })
+
+    test('creates a 2 dimension array from a single dimension array with a specific width', () => {
+      let cells = (new Array(3)).fill(null)
+      let matrix = matrixize(cells, 1)
+
+      expect(matrix.length).toBe(3)
+      expect(matrix[0].length).toBe(1)
+      expect(matrix[1].length).toBe(1)
+      expect(matrix[2].length).toBe(1)
+    })
+
+  })
+
+
+
 })
