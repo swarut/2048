1db1901b6bf72038bfa9c53fe87ff4c3e4684867
Warut Surapat <s.warut@gmail.com>
2018-05-08 00:55:38 +0900

    Prepare case for movement.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,10 @@
-import { START } from '../actions/index'
+import {
+  START,
+  MOVE_LEFT,
+  MOVE_RIGHT,
+  MOVE_UP,
+  MOVE_DOWN
+} from '../actions/index'
 
 const startupCells = (new Array(16)).fill(null)
 
@@ -13,6 +19,10 @@ const reducer = (state = defaultState, action) => {
       return {
         isStarted: true
       }
+    case MOVE_LEFT:
+    case MOVE_RIGHT:
+    case MOVE_UP:
+    case MOVE_DOWN:
     default:
       return state
   }
